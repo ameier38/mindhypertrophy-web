d40e316e2961a3dafe3904fce95a4f3b61cb6f79
Andrew Meier <ameier38@gmail.com>
2017-03-02 23:05:06 -0500

    redux/initial: update markdown

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,64 +1,46 @@
 // based on react-remarkable
 // https://github.com/acdlite/react-remarkable
 
-import React, { Component } from 'react';
-var MarkdownIt = require('markdown-it')
-var mk = require('markdown-it-katex')
-var hljs = require('highlight.js'); // https://highlightjs.org/
+import React, { Component } from 'react'
+import MarkdownIt from 'markdown-it'
+import katex from 'markdown-it-katex'
+import hljs from 'highlight.js'
 
 class Markdown extends Component{
   render() {
-    var Container = this.props.container;
-
     return (
-      <Container>
+      <div>
         {this.content()}
-      </Container>
+      </div>
     )
   }
-  componentWillUpdate(nextProps, nextState) {
-    if (nextProps.options !== this.props.options) {
-      this.md = new MarkdownIt(nextProps.options)
-      this.md.use(mk)
-    }
-  }
   content() {
-    if (this.props.source) {
-      return <span dangerouslySetInnerHTML={{ __html: this.renderMarkdown(this.props.source) }} />
-    }
-    else {
-      return React.Children.map(this.props.children, child => {
-        if (typeof child === 'string') {
-          return <span dangerouslySetInnerHTML={{ __html: this.renderMarkdown(child) }} />
-        }
-        else {
-          return child
-        }
-      })
-    }
+    return React.Children.map(this.props.children, child => {
+      if (typeof child === 'string') {
+        return <span dangerouslySetInnerHTML={{ __html: this.renderMarkdown(child) }} />
+      }
+      else {
+        return child
+      }
+    })
   }
   renderMarkdown(source) {
-    if (!this.md) {
-      this.md = new MarkdownIt(this.props.options);
-      this.md.use(mk)
+    if (!this.markdown) {
+      this.markdown = new MarkdownIt({
+        highlight: (str, lang) => {
+          if (lang && hljs.getLanguage(lang)) {
+            try {
+              return hljs.highlight(lang, str).value;
+            } catch (__) {}
+          }
+          return '' // use external default escaping
+        }
+      })
+      this.markdown.use(katex)
     }
-    return this.md.render(source)
+    return this.markdown.render(source)
   }
 
-}
-Markdown.defaultProps = {
-    options: {
-      highlight: (str, lang) => {
-        if (lang && hljs.getLanguage(lang)) {
-          try {
-            return hljs.highlight(lang, str).value;
-          } catch (__) {}
-        }
-        return '' // use external default escaping
-      }
-    },
-  container: 'div'
-    
 }
 
 export default Markdown
